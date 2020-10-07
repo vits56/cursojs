ef3da5ce60324b5ca2e82cb4393bb31a7790262b
vits56 <bastosduartebruno@outlook.com>
2020-10-07 00:43:02 -0300

    Create ex008.js

---
+++ aula11/ex008.js
@@ -0,0 +1,4 @@
+var vel = 60.5
+console.log(`a velocidade do seu carro é ${vel} KM/H. `)
+console.log('voce ultrapassou a velocidade permitida, MULTADO !!!')
+console.log('dirija usando sempre o cinto de segurança')
